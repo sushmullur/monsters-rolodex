9c87312e1c070e08e42874ad22b11ff6ab38feba
sushpicious <2011658@apps.nsd.org>
2022-12-19 21:57:41 -0800

    Fetches the monsters from provided url.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,20 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda'
-        }, 
-        {
-          name: 'Frank'
-        }, 
-        {
-          name: 'Jacky'
-        }, 
-        {
-          name: 'Andrei'
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
+    }
+    ));
   }
 
   render() {
@@ -30,7 +29,11 @@ class App extends Component {
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>;
+            return <div key={monster.id}>
+                <h1>
+                  {monster.name}
+                </h1>
+              </div>;
           })
         }
       </div>
