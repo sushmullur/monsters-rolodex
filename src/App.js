001d2bc95ef020ee70a0d4e7d3d8f53ec30e9170
sushpicious <2011658@apps.nsd.org>
2022-12-20 00:12:00 -0800

    Fixed filtering reversal for search box

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -25,21 +26,21 @@ class App extends Component {
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='Search monsters' onChange = {(event) => {
-          console.log(event.target.value);
-          const searchString = event.target.value.toLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLowerCase().includes(searchString);
-          });
+          const searchField = event.target.value.toLowerCase();
 
           this.setState(() => {
-            return { monsters: filteredMonsters };
+            return { searchField };
           });
         }}/>
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <div key={monster.id}>
                 <h1>
                   {monster.name}
