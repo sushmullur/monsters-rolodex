eb04506286ae1dec075624547106bcad4ed5a0f1
sushpicious <2011658@apps.nsd.org>
2022-12-22 10:12:41 -0800

    Styled the card list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,27 @@
 import { Component } from "react";
+import './card-list.styles.css';
 
 class CardList extends Component {
+
     render() {
-        console.log('Render from card list');
         const { monsters } = this.props;
-        return <div className = 'card-list'>
-            {monsters.map(monster => (
-                <h1 key={monster.id}>{monster.name}</h1>
-            ))}
-            </div>;
+
+        return (
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster;
+                    return (
+                        <div className='card-container' key={id}>
+                            <img 
+                                alt={`monster ${name}`}
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                            />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )})}
+            </div>
+        );
     }
 }
 
