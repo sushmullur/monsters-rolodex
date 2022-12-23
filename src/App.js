48d1b753e1e8c6b1dc447922014b147f1e3eb495
sushpicious <2011658@apps.nsd.org>
2022-12-23 12:18:45 -0800

    Fixed inifinite render problem

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import './App.css';
 import SearchBox from './components/search-box/search-box.component';
 
 const App = () => {
-  console.log('render');
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const[filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  console.log('render');
-
-  fetch('https://jsonplaceholder.typicode.com/users')
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+  });
+
+  setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   }
 
-  const filteredMonsters = monsters.filter((monster) => {
-          return monster.name.toLowerCase().includes(searchField);
-      });
+  
 
 
   return (
